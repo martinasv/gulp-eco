eb0932f84f3896265fd0618feb69aa8bdb265b1b
martinas <martinas@martinas.info>
2022-01-11 13:47:17 +0200

    Restored bstst fork

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var path = require('path'),
-    through = require('through2'),
-    gutil = require('gulp-util'),
-    eco = require('eco');
+  through = require('through2'),
+  gutil = require('gulp-util'),
+  eco = require('eco');
 
 module.exports = function (opt) {
 
@@ -20,7 +20,7 @@ module.exports = function (opt) {
     JSTpath = JSTpath.replace(re, '');
 
     var str = file.contents.toString();
-    output = eco.compile(str) + ';';
+    output = eco.compile(str);
     output = 'window.' + opt.namespace + '["' + JSTpath + '"] = ' + output + '\n';
     output = "if (!window." + opt.namespace + ") {\n  window." + opt.namespace + " = {};\n}\n" + output;
 
